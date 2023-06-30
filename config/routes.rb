fa1385f6b2c39ef052682bdd2626d0f9af55b655
stephenkati <stephenkatuli88@gmail.com>
2023-06-30 19:08:23 +0300

    add functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root "recipes#public_recipes"
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :public_recipes] do
     member do
       patch :toggle_public
     end
+    resources :recipe_foods, only: [:new, :create, :edit, :update,  :destroy]
   end
 end
